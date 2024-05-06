eda5cb3cdc0c01fccc60c4c70507474f7448c9c5
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-05-06 13:32:53 +0200

    Added middleware and routes for authentication and pages

--- src/app.js
+++ src/app.js
@@ -2,21 +2,32 @@
  * ------------------------------
  *            IMPORTS
  * ------------------------------
- */
+*/
+
 import express from "express";
 import { create } from "express-handlebars";
+import bodyParser from "body-parser";
 import { VIEWS_PATH, PORT } from "./consts.js";
+import cookieParser from "cookie-parser";
 
 
-// middleware
-// controllers
+// Middleware
+
+
+// Controllers
+
+
+// Routes
 
 /**
  * ------------------------------
  *       CONFIGURATION
  * ------------------------------
- */
+*/
 const app = express();
+
+// Make use of the cookie parser 🍪 middleware
+app.use(cookieParser());
 
 const hbs = create({
     extname: "hbs",
@@ -26,17 +37,46 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", VIEWS_PATH);
 
+// Static files
+app.use(express.static("public"));
+
+// View the body of the request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 /**
  * ------------------------------
  *            ROUTING
  * ------------------------------
- */
+*/
+
+// Auth routes
+app.get("/login", () => {});
+app.post("/login", () => {});
+
+app.get("/register", () => {});
+app.post("/register", () => {});
+
+app.get("/logout", () => {});
+
+
+// Page Routes
+app.get("/", () => {});
+
+
+// API Routes
+app.get("/api", () => {});
+
+// 404 Route
+app.use('*', () => {});
+
 
 /**
  * ------------------------------
  *        START SERVER
  * ------------------------------
- */
+*/
 app.listen(PORT, () => {
     console.log(`Application is running on http://localhost:${PORT}/.`);
 });
