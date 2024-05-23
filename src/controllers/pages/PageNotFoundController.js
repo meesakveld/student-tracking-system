ffb545fc69715146629ab3d91153cea9a303c057
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-05-23 16:22:51 +0200

    Edited not found page

--- src/controllers/pages/PageNotFoundController.js
+++ src/controllers/pages/PageNotFoundController.js
@@ -5,7 +5,16 @@
 */
 
 export const pageNotFound = (req, res) => {
+    const firstname = "Philippe";
+    const lastname = "De Pauw - Waterschoot";
+    const role = "Admin";
+
     const data = {
+        user: {
+            firstname,
+            lastname,
+            role,
+        },
         error: 'Page Not Found',
         message: 'Pagina niet gevonden.'
     };
