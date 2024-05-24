77c464c9148cf2b67ede51c0b80f7038cf6935bd
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-05-24 09:18:45 +0200

    Added role authorization to /users route

--- src/routes/private-routes/index.js
+++ src/routes/private-routes/index.js
@@ -11,6 +11,7 @@ import * as ctr from "../../controllers/pages/index.js";
 
 // Middleware
 import jwtAuth from "../../middleware/jwtAuth.js";
+import roleAuth from "../../middleware/roleAuth.js";
 
 
 /**
@@ -22,8 +23,6 @@ import jwtAuth from "../../middleware/jwtAuth.js";
 const router = Express.Router();
 
 router.use(jwtAuth);
-// TODO: Make function to check what role and optionally function to check permissions
-
 
 /**
  * ------------------------------
@@ -32,7 +31,7 @@ router.use(jwtAuth);
 */
 
 router.get('/', ctr.dashboardPage);
-router.get('/users', ctr.usersPage);
+router.get('/users', roleAuth(["employee", "student"], ["teacher"]), ctr.usersPage);
 router.get('/users/:id', ctr.userPage);
 router.get('/student/:id', ctr.studentPage);
 router.get('/student/:id/:detail', ctr.studentDetailPage);
