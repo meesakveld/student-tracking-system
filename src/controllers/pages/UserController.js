663aa9d9456f96ae15bc8068efc41abaf9a93ac0
pgm-tristanderidder <tristan.deridder@student.arteveldehs.be>
2024-05-24 15:22:02 +0200

    added student info page functionality

--- src/controllers/pages/UserController.js
+++ src/controllers/pages/UserController.js
@@ -4,27 +4,52 @@
  * ------------------------------
  */
 
-export const userPage = (req, res) => {
-    const userInfo = {
-        firstName: "Mees",
-        lastName: "Akveld",
-        email: "mees.ak@student.arteveldehs.be",
-        class: "PGM1-C",
-        studyYear: "2023-2024",
-        status: "Werkstudent",
-        role: "Ambassadeur",
-        coach: "Isabelle Volckaert",
-        workCoach: "Viktor Verhaeghe",
-        workMentor: "Lander De Vos",
-        labels: ["Ambassadeur", "Stuver"]
-    };
+import { getStudentById } from "../../services/models/Student.js";
+import Student from "../../models/Student.js";
 
-    const data = {
-        user: req.user,
-        userInfo,
-    };
 
-    res.render('user', data);
+export const userPage = async (req, res) => {
+
+    try {
+
+        const id = parseInt(req.params.id);
+
+        const student = await getStudentById(id, '[user.role, labels, class, status_registration.status, trajectory_coach.user, workplace_coach, workplace_mentor]');
+        console.log(student);
+
+
+        // show more recent status value -> status_registration.status.title
+        const userInfo = {
+            firstName: student.user.firstname,
+            lastName: student.user.lastname,
+            email: student.user.email,
+            class: student.class.name,
+            status: student.status_registration[0]?.status.title || "-",
+            role: student.user.role.title || "-",
+            coach: student.trajectory_coach?.users || "-",
+            workCoach: student.workplace_coach?.employees || "-",
+            workMentor: student.workplace_mentor?.employees || "-",
+            labels: student.labels.map(label => label.title) || null,
+        };
+
+        const data = {
+            user: req.user,
+            userInfo,
+            returnUrl: "/"
+        };
+
+        res.render('user', data);
+
+    } catch (error) {
+        const errorObj = {
+            user: req.user,
+            error: {
+                message: error.message,
+                code: 500
+            }
+        }
+        res.status(500).render('error', errorObj);
+    }
 };
 
 export default userPage;
