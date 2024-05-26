8a8fc9f6fa801b3ecfcffbbf3478313fdf1c3058
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-05-27 00:10:30 +0200

    Add employeed function authorization to education programme routes

--- src/routes/api/educationProgrammeRoutes.js
+++ src/routes/api/educationProgrammeRoutes.js
@@ -9,6 +9,10 @@ import Express from "express";
 // Models
 import EducationProgramme from "../../models/EducationProgramme.js";
 
+// Utils
+import { employeeFunctionAuth } from "../../utils/employeeFunctionAuth.js";
+import Employee from "../../models/Employee.js";
+
 /**
  * ------------------------------
  *        CONFIGURATION
@@ -27,11 +31,23 @@ const router = Express.Router();
 // API Todos routes
 router.get('/', async (req, res) => {
     
+    let hasFullAccess = true;
+    if (req.query.employee_id) {
+        const employee = await Employee.query().findById(req.query.employee_id).withGraphFetched('functions');
+        hasFullAccess = employeeFunctionAuth(employee.functions, ["admin"]);
+    }
+
     if (!req.query.academic_year) {
         return res.status(400).json({ errors: [{ msg: "Geen academisch jaar opgegeven" }] });
     }
 
     const education_programmes = await EducationProgramme.query()
+        .joinRelated(!hasFullAccess && 'courses.employees')
+        .where(builder => {
+            if (!hasFullAccess && req.query.employee_id) {
+                builder.where('courses:employees.id', req.query.employee_id)
+            }
+        })
         .where('academic_year', req.query.academic_year)
 
     if (!education_programmes || education_programmes.length === 0) {
