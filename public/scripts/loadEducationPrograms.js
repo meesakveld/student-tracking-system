38ac287c7e45cd140811a92241c2323c26d131e8
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-05-26 10:25:36 +0200

    Added try catch

--- public/scripts/loadEducationPrograms.js
+++ public/scripts/loadEducationPrograms.js
@@ -1,28 +1,34 @@
 const loadEducationPrograms = () => {
 
     const academicYear = document.getElementById('filterAcademicYear');
+    const educationProgramme = document.getElementById('filterProgram')
+    
     academicYear.addEventListener('change', async () => {
-        const academicYear = document.getElementById('filterAcademicYear').value;
-        const educationProgramme = document.getElementById('filterProgram')
+        try {
+            const academicYearValue = academicYear.value;
 
-        if (!academicYear) {
+            if (!academicYearValue) {
+                educationProgramme.innerHTML = '<option value="">Alle opleidingen</option>';
+                educationProgramme.setAttribute('disabled', true);
+                return;
+            }
+            
+            const response = await fetch(`/api/education-programmes?academic_year=${academicYearValue}`);
+            const data = await response.json();
+
+            const options = data.map(program => {
+                return `<option value="${program.code}">${program.title}</option>`;
+            });
+
+            options.unshift('<option value="">Alle opleidingen</option>');
+
+            educationProgramme.innerHTML = ''
+            educationProgramme.innerHTML = options.join('');
+            educationProgramme.removeAttribute('disabled');
+        } catch (error) {
             educationProgramme.innerHTML = '<option value="">Alle opleidingen</option>';
             educationProgramme.setAttribute('disabled', true);
-            return;
         }
-        
-        const response = await fetch(`/api/education-programmes?academic_year=${academicYear}`);
-        const data = await response.json();
-
-        const options = data.map(program => {
-            return `<option value="${program.code}">${program.title}</option>`;
-        });
-
-        options.unshift('<option value="">Alle opleidingen</option>');
-
-        educationProgramme.innerHTML = ''
-        educationProgramme.innerHTML = options.join('');
-        educationProgramme.removeAttribute('disabled');
     });
 };
 
