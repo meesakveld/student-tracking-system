37380247ebc274cb86e557df86d06691dee1dcce
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-05-26 10:21:25 +0200

    Added API route for fetching education programmes by academic year

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -9,6 +9,7 @@ import Express from "express";
 // Controllers
 import userRoutes from "./userRoutes.js";
 import authRoutes from "./authRoutes.js";
+import educationProgramme from "./educationProgrammeRoutes.js";
 
 // Middleware
 import { checkUserToken } from "../../controllers/api/AuthController.js";
@@ -30,10 +31,10 @@ const router = Express.Router();
 
 // —— Public routes | Auth routes ——
 router.use('/auth', authRoutes)
+router.use('/education-programmes', educationProgramme)
 
 // —— Private routes ——
-router.use(checkUserToken)
-router.use('/user', userRoutes)
+router.use('/user', checkUserToken, userRoutes)
 
 
 export default router;
