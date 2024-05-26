a0705b29fc505fb0f024ba51d41d094a40a555d6
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-05-26 10:27:08 +0200

    Improved filtering logic in usersPage controller

--- src/controllers/pages/UsersController.js
+++ src/controllers/pages/UsersController.js
@@ -4,20 +4,28 @@
  * ------------------------------
 */
 
-import { getAllStudents } from "../../services/models/Student.js";
-import { getAllEmployees } from "../../services/models/Employee.js";
-import { getAllEducationProgrammes } from "../../services/models/EducationProgramme.js";
 import Student from "../../models/Student.js";
 import Employee from "../../models/Employee.js";
+import EducationProgramme from "../../models/EducationProgramme.js";
 
 export const usersPage = async (req, res) => {
 
     const filterRole = req.query.filterRole;
     const filterProgram = req.query.filterProgram;
     const filterStatus = req.query.filterStatus === "active" ? true : req.query.filterStatus === "inactive" ? false : null;
+    const filterAcademicYear = req.query.filterAcademicYear;
 
-    const programmes = await getAllEducationProgrammes();
-    const programmesOptions = programmes.map(programme => ({ value: programme.slug, label: `${programme.title} - ${programme.code}` }));
+    const academicYearsQuery = await EducationProgramme.query().distinct('academic_year').select('academic_year');
+    const academicYears = academicYearsQuery.map(academicYear => academicYear.academic_year);
+    const academicYearsOptions = academicYears.map(academicYear => ({ value: academicYear, label: academicYear, selected: academicYear === filterAcademicYear}));
+
+    const programmes = !filterAcademicYear ? [] : await EducationProgramme.query()
+        .where(builder => {
+            if (filterAcademicYear) {
+                builder.where('academic_year', filterAcademicYear);
+            }
+        });
+    const programmesOptions = programmes.map(programme => ({ value: programme.code, label: `${programme.title} - ${programme.code}`, selected: programme.code === filterProgram }));
 
     // ——— FILTERS ———
     const userFilters = [
@@ -33,13 +41,23 @@ export const usersPage = async (req, res) => {
             ]
         },
         {
-            id: "filter3",
+            id: "filterAcademicYear",
+            name: "filterAcademicYear",
+            labelText: "Filter op academisch jaar:",
+            options: [
+                { value: "", label: "Selecteer academisch jaar" },
+                ...academicYearsOptions
+            ]
+        },
+        {
+            id: "filterProgram",
             name: "filterProgram",
             labelText: "Filter op opleiding:",
             options: [
                 { value: "", label: "Alle opleidingen" },
                 ...programmesOptions
-            ]
+            ],
+            disabled: !filterAcademicYear
         },
         {
             id: "filter4",
@@ -47,8 +65,8 @@ export const usersPage = async (req, res) => {
             labelText: "Filter op status:",
             options: [
                 { value: "", label: "Alle statussen" },
-                { value: "active", label: "Actief" },
-                { value: "inactive", label: "Inactief" }
+                { value: "active", label: "Actief", selected: filterStatus === true },
+                { value: "inactive", label: "Inactief", selected: filterStatus === false }
             ]
         }
     ];
@@ -58,29 +76,48 @@ export const usersPage = async (req, res) => {
     let employees = [];
 
     // Sla een parameter over als deze niet is ingevuld
-    const test = await Student.query()
-        .withGraphFetched('[user(filterUser).role, education_programmes(filterProgram)]')
-        .modifiers({
-            filterUser(builder) {
-                if (filterStatus !== null) {
-                    builder.where('is_active', filterStatus);
-                }
-                if (filterRole !== undefined) {
-                    builder.join('roles', 'users.role_id', 'roles.id')
-                        .where('roles.title', filterRole);
-                }
-            },
-            filterProgram(builder) {
-                if (filterProgram !== undefined) {
-                    builder.where('slug', filterProgram);
-                }
+    students = await Student.query()
+        .withGraphFetched('[user.role, education_programmes]')
+        .joinRelated('user')
+        .where(builder => {
+            if (filterStatus !== null) {
+                builder.where('user.is_active', filterStatus);
             }
-        });
+        })
+        .joinRelated(filterProgram || filterAcademicYear ? 'education_programmes' : '')
+        .where(builder => {
+            if (filterAcademicYear) {
+                builder.where('education_programmes.academic_year', filterAcademicYear);
+            }
+        })
+        .where(builder => {
+            if (filterProgram) {
+                builder.where('education_programmes.code', filterProgram);
+            }
+        })
 
-    console.log('test', test);
+    employees = await Employee.query()
+        .withGraphFetched('[user.role, education_programmes]')
+        .joinRelated('user')
+        .where(builder => {
+            if (filterStatus !== null) {
+                builder.where('user.is_active', filterStatus);
+            }
+        })
+        .joinRelated(filterProgram || filterAcademicYear ? 'education_programmes' : '')
+        .where(builder => {
+            if (filterAcademicYear) {
+                builder.where('education_programmes.academic_year', filterAcademicYear);
+            }
+        })
+        .where(builder => {
+            if (filterProgram) {
+                builder.where('education_programmes.code', filterProgram);
+            }
+        })
 
-    students = await getAllStudents('[user.role, education_programmes]');
-    employees = await getAllEmployees('[user.role, education_programmes]');
+    if (filterRole === "student") employees = [];
+    if (filterRole === "employee") students = [];
 
     let users = [...students, ...employees];
     users.sort((a, b) => a.user.lastname < b.user.lastname ? -1 : 1);
