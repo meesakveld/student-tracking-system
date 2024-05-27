d343f54963b3d9ddfa39e0cd5b7658a6157c93d7
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-05-27 19:30:11 +0200

    Refactored studentIdAuth middleware to handle both id and studentId parameters

--- src/middleware/authentication/studentIdAuth.js
+++ src/middleware/authentication/studentIdAuth.js
@@ -1,9 +1,19 @@
 export default (req, res, next) => {
 
-    if (req.user.role.title === "student" && parseInt(req.params.id) !== req.user.id) {
-        res.render("error", { user: req.user, error: { code: 403, message: 'Je hebt geen toegang tot deze pagina' }})
-    } else {
-        next()
+    if (req.params.id) {
+        if (req.user.role.title === "student" && parseInt(req.params.id) !== req.user.id) {
+            res.render("error", { user: req.user, error: { code: 403, message: 'Je hebt geen toegang tot deze pagina' }})
+        } else {
+            next()
+        }
+    }
+
+    if (req.params.studentId) {
+        if (req.user.role.title === "student" && parseInt(req.params.studentId) !== req.user.student.id) {
+            res.render("error", { user: req.user, error: { code: 403, message: 'Je hebt geen toegang tot deze pagina' }})
+        } else {
+            next()
+        }
     }
 
 }
