293aa6473311a13f25e449478eb8ea08afe13e32
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-05-27 18:03:19 +0200

    Added dynamic dashboard links based on user role and functions

--- src/controllers/pages/DashboardController.js
+++ src/controllers/pages/DashboardController.js
@@ -4,9 +4,39 @@
  * ------------------------------
 */
 
-export const dashboardPage = (req, res) => {
+import { employeeFunctionAuth } from '../../utils/employeeFunctionAuth.js'
+
+export const dashboardPage = async (req, res) => {
+
+    const dashboardData = await import('../../data/dashboard.json', { assert: { type: 'json' } }).then(data => data.default);
+    const dashboardUserData = dashboardData.map(value => {
+        if (!value.roles.includes(req.user.role.title)) return
+        if (req.user.employee) {
+            if (!employeeFunctionAuth(req.user.employee.functions, value.functions)) return
+        }
+
+        if (value.url.includes("{id}")) {
+            value.url = value.url.replace("{id}", req.user.id);
+        }
+
+        if (value.url.includes("{student_id}")) {
+            value.url = value.url.replace("{student_id}", req.user.student.id);
+        }
+
+        if (value.url.includes("{employee_id}")) {
+            value.url = value.url.replace("{employee_id}", req.user.employee.id);
+        }
+
+        return {
+            title: value.title,
+            url: value.url
+        }
+
+    }).filter(value => value !== undefined);
+
     const data = {
         user: req.user,
+        dashboardLinks: dashboardUserData
     };
 
     res.render("dashboard", data);
