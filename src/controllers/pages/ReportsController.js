d6c34763dddfd656c5249d10fd542181f1cdfd7a
meesakveld <meesakveld@gmail.com>
2024-06-10 13:03:29 +0200

    Updated ReportsController to fetch student information in comments

--- src/controllers/pages/ReportsController.js
+++ src/controllers/pages/ReportsController.js
@@ -3,8 +3,8 @@ import { formatDate } from "../../utils/formatDate.js";
 
 export const reportsPage = async (req, res, next) => {
     try {
-        // Get the id of the logged-in employee and get the comment type -> possibilities: course OR coaching
         const employeeId = req.user.employee.id;
+
         // Extract the specific word (e.g., 'course') from the URL
         const url = req.originalUrl; // get the full URL
         const pathSegments = url.split('/'); // split the URL into segments
@@ -12,7 +12,7 @@ export const reportsPage = async (req, res, next) => {
 
         // Initialize the query with the logged-in employee's ID and the extracted word
         let comments = await Comment.query()
-            .withGraphFetched('[employee.[user, functions], course]')
+            .withGraphFetched('[employee.[user, functions], course, student.[user]]')
             .where('employee_id', employeeId)
             .where('tag', reportType)
             .orderBy('created_at', 'desc');
@@ -20,7 +20,7 @@ export const reportsPage = async (req, res, next) => {
         // Format comments for rendering
         const formattedComments = comments.map((comment) => {
             return {
-                title: `${formatDate(comment.created_at)}${comment.course ? ` — ${comment.course.name}` : ''} — ${comment.employee.user.firstname} ${comment.employee.user.lastname}`,
+                title: `${formatDate(comment.created_at)}${comment.course ? ` — ${comment.course.name}` : ''} — ${comment.student.user.firstname} ${comment.student.user.lastname}`,
                 text: comment.comment,
                 returnUrl: `${reportType === "course" ? '/course-reports' : reportType === "personal" ? '/personal-reports' : '/coaching-reports'}/`,
                 ...comment
